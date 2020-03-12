05ce8c14a9410e07a22b77342218195040a6fd14
lh008 <paintluis@gmail.com>
2020-03-12 17:14:48 -0500

    Adding 15 unfinished

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - Entry point
+ * Desc: binary_tree_is_full function
+ * @tree: binary_tree_t type
+ * Return: Function that checks if a binary tree is full
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+
+	if (!tree)
+		return (0);
+
+	
+}
