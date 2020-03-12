6aba1e1ee50e7d4975a49c0967553edafb102677
lh008 <paintluis@gmail.com>
2020-03-12 11:53:52 -0500

    Adding 12. Leaves}

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Entry point
+ * Desc: binary_tree_leaves function
+ * @tree: binary_tree_t type
+ * Return: Function that counts the leaves in a binary tree
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t ct = 0;
+	const binary_tree_t *t;
+
+	t = tree;
+
+	if (!tree)
+		return (0);
+
+	if (t->left == NULL && t->right == NULL)
+		return (1);
+	else
+		ct = binary_tree_leaves(t->left) + binary_tree_leaves(t->right);
+
+	return (ct);
+}
