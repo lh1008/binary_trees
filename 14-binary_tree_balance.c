3087e4f4a01d9a09d260bb8ae29b1b9feacfadc9
lh008 <paintluis@gmail.com>
2020-03-12 16:12:54 -0500

    Adding 14. Balance factor

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,71 @@
+#include "binary_trees.h"
+
+
+int _max(int n, int n2);
+int _height(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree);
+
+/**
+ * binary_tree_balance - Entry point
+ * Desc: binary_tree_balance function
+ * @tree: binary_tree_t type
+ * Return: Function that counts the nodes with at least 1
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t left = 0, right = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		left = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right = binary_tree_height(tree->right) + 1;
+
+	return (left - right);
+}
+
+/**
+ * binary_tree_height - Entry point
+ * Desc: binary_tree_height function
+ * @tree: binary_tree_t type
+ * Return: Function that returns the height of a binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (_height(tree) - 1);
+}
+
+/**
+ * _height - Entry point
+ * Desc: _height function
+ * @tree: binary_tree_t type
+ * Return: Function that measures the height of a binary tree
+ */
+int _height(const binary_tree_t *tree)
+{
+
+	if (!tree)
+		return (0);
+
+	return (1 + _max(_height(tree->left), _height(tree->right)));
+}
+
+/**
+ * _max - Entry point
+ * Desc: _max function
+ * @n: int type
+ * @n2: int type
+ * Return: Function that measures if an int is greater than another one
+ */
+int _max(int n, int n2)
+{
+	if (n > n2)
+		return (n);
+	else
+		return (n2);
+}
