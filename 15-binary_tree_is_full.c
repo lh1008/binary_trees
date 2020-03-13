623b37315cd1561f134dd7fa010608830c469a73
lh008 <paintluis@gmail.com>
2020-03-13 10:22:13 -0500

    Adding 15. Is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,9 +8,21 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	const binary_tree_t *t;
+	int c;
+
+	t = tree;
 
 	if (!tree)
 		return (0);
 
-	
+	if (t->left == NULL && t->right == NULL)
+		return (1);
+
+	c = (binary_tree_is_full(t->left) && binary_tree_is_full(t->right));
+
+	if ((t->left) && (t->right))
+		return (c);
+
+	return (c);
 }
